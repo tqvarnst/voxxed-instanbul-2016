1e967f6b50cdd8e5432d7e2c4809d43d41fac710
tqvarnst <tqvarnst@redhat.com>
2016-05-06 20:17:27 +0200

    English version

--- projects/simpleApp/src/main/java/com/example/service/GreetingService.java
+++ projects/simpleApp/src/main/java/com/example/service/GreetingService.java
@@ -6,10 +6,10 @@ import javax.enterprise.context.ApplicationScoped;
 public class GreetingService {
 
 	public String sayHello() {
-//		return "hello"; //English
+		return "hello"; //English
 //		return "hej"; // Swedish
 // 		return "marhaba"; // Arabic
- 		return "merhaba"; // Turkish
+// 		return "merhaba"; // Turkish
 //		return "hole"; // Spanish
 //		return "namaste"; // Hindi
 //		return "konnichiwa"; //Japanese
